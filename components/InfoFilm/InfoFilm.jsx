f796c5b990ce8c3865554b3dff7a02b4d28dd80a
George <92686556+GrudTrigger@users.noreply.github.com>
2023-09-11 20:40:36 +0300

    upload InfoFilm componnt

--- components/InfoFilm/InfoFilm.jsx
+++ components/InfoFilm/InfoFilm.jsx
@@ -1,10 +1,10 @@
 import styles from './InfoFilm.module.css';
 import Image from "next/image";
 import {formatTime} from "@/components/helpers/helpers";
+import Link from "next/link";
 
 export const InfoFilm = ({film}) => {
     console.log(film)
-    const trailer = film.videos.trailers[0].url;
     const srcImage = film.poster.url;
     const title = film.name
     const timeFilm = formatTime(film.movieLength)
@@ -14,10 +14,10 @@ export const InfoFilm = ({film}) => {
             <div className={styles.wrapperFilm}>
                 <div className={styles.poster}>
                     <div>
-                        <Image src={srcImage} alt={'Постер'} width={719} height={404} />
+                        <Image className={styles.posterImage} src={srcImage} width={719} height={680} alt={'Постер'}/>
                     </div>
                 </div>
-                <div>
+                <div className={styles.containerInfo}>
                     <h1 className={styles.titleFilm}>{title}</h1>
                     <div className={styles.paramsFilm}>
                         <div className={styles.paramsList}>
@@ -26,21 +26,60 @@ export const InfoFilm = ({film}) => {
                             <div className={styles.params}>{film.ageRating}+</div>
                         </div>
                         <div className={styles.paramsList}>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span></span>
+                            <div className={styles.params}>{film.countries[0].name}</div>
+                            {film.genres.map((genre, index) => {
+                                 return <div className={styles.params} key={index}>{genre.name[0].toUpperCase() + genre.name.slice(1)}</div>
+                            })}
                         </div>
-                        <div>
-                            <div className={styles.ratingFilm}></div>
-                            <div className={styles.actorsFilm}></div>
+                        <div className={styles.actorsList}>
+                            <div>
+                                <div className={styles.ratingFilm}>
+                                    <div className={styles.test}>{film.rating.imdb}</div>
+                                    <div className={styles.ratingDescr}>Рейтинг imdb</div>
+                                </div>
+                            </div>
+                            {film.persons.map((person, index) => {
+                                if( index < 3) {
+                                    return <div key={index} className={styles.actors}>
+                                        <Image className={styles.actorImage} src={person.photo} alt={person.name} width={56} height={56}/>
+                                        <div className={styles.actor}>{person.name}</div>
+                                    </div>
+                                }
+                            })}
                         </div>
-                        <div>
-                            <p className={styles.descrFilm}></p>
+                        <div className={styles.descrWrapper}>
+                            <p className={styles.descrFilm}>{film.description}</p>
+                            <div className={styles.borderLink}>
+                                <Link className={styles.trailerLink} href={film.videos.trailers[0].url}>Смотреть трейлер</Link>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div className={styles.wrapperFilms}>
+                <h3 className={styles.subTitleFilm}>С фильмом "{film.name}" смотрят</h3>
+                <div className={styles.wrapperItems}>
+                    {film.similarMovies.map((f, index) => {
+                        if(index < 7){
+                            return <div key={index} className={styles.film}>
+                                <Link className={styles.linkFilm} href={`/films/${f.id}`}>
+                                    <Image className={styles.filmImage} src={f.poster.url} alt={f.name} width={153} height={235}/>
+                                    <p className={styles.filmTitle}>{f.name}</p>
+                                </Link>
+                            </div>
+                        }
+                    })}
+                </div>
+            </div>
+
+            <div className={styles.wrapperFacts}>
+                <h3 className={styles.titleFact}>Знаете ли вы, что</h3>
+                {film.facts.map((fact, index) => {
+                    return <p className={styles.paragrafFact} key={index}>{fact.value}</p>
+                })}
+            </div>
+
         </div>
     )
 }
